64cc1050f817a68a8e9c98892372ba5cbec06132
Zipdox <zipdox@protonmail.com>
2020-08-08 00:22:38 +0200

    Fixed filename and download button creation. Updated readme.

--- README.md
+++ README.md
@@ -3,10 +3,19 @@ A browser userscript to download tiktok videos without watermark.
 
 Tiktok downloader.
 
-[Click here to install](https://github.com/Zipdox/tiktok-download-userscript/raw/master/tiktok-dl.user.js)
+# Installation
+Install a userscript manager:
+
+  * [Greasemonkey (Firefox)](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/)
+  * [Tampermonkey (Chrome/Chromium)](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)
+
+Then click [here](https://github.com/Zipdox/tiktok-download-userscript/raw/master/tiktok-dl.user.js) to install the userscript.
 
 # Usage
-A download button will appear next to the other share buttons when viewing a post. If it doesn't appear, refresh the page or open it in a new tab.
+A download button will appear next to the other share buttons when viewing a post.
 
 # Credits
 Thanks to [tiktok-scraper](https://github.com/drawrowfly/tiktok-scraper) for watermarkless video extraction trick.
+
+# License
+This code is licensed under the [GPLv2](https://www.gnu.org/licenses/old-licenses/gpl-2.0.html).
--- tiktok-dl.user.js
+++ tiktok-dl.user.js
@@ -1,12 +1,25 @@
 // ==UserScript==
 // @name     TikTok Downloader
-// @version  1.0
+// @version  1.1
 // @grant    none
-// @match    https://www.tiktok.com/@*/*
-// @run-at   document-idle
+// @match    https://www.tiktok.com/@*
+// @run-at   document-end
 // @author   Zipdox
 // @homepageURL https://github.com/Zipdox/tiktok-download-userscript
+// @require      https://raw.githubusercontent.com/narcolepticinsomniac/arrive/master/minified/arrive.min.js
 // ==/UserScript==
+
+
+document.arrive(".share-group", function(){
+  if(this.getElementsByClassName('downloadButton').length > 0) return;
+  let shareButtons = this.getElementsByTagName('a');
+  let downloadButton = document.createElement('a');
+  this.insertBefore(downloadButton, shareButtons[4]);
+  downloadButton.innerHTML = '<span><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"  viewBox="0 0 477.867 477.867" style="enable-background:new 0 0 477.867 477.867;" xml:space="preserve"><g> <g> <path d="M443.733,307.2c-9.426,0-17.067,7.641-17.067,17.067v102.4c0,9.426-7.641,17.067-17.067,17.067H68.267 c-9.426,0-17.067-7.641-17.067-17.067v-102.4c0-9.426-7.641-17.067-17.067-17.067s-17.067,7.641-17.067,17.067v102.4 c0,28.277,22.923,51.2,51.2,51.2H409.6c28.277,0,51.2-22.923,51.2-51.2v-102.4C460.8,314.841,453.159,307.2,443.733,307.2z"/> </g></g><g> <g> <path d="M335.947,295.134c-6.614-6.387-17.099-6.387-23.712,0L256,351.334V17.067C256,7.641,248.359,0,238.933,0 s-17.067,7.641-17.067,17.067v334.268l-56.201-56.201c-6.78-6.548-17.584-6.36-24.132,0.419c-6.388,6.614-6.388,17.099,0,23.713 l85.333,85.333c6.657,6.673,17.463,6.687,24.136,0.031c0.01-0.01,0.02-0.02,0.031-0.031l85.333-85.333 C342.915,312.486,342.727,301.682,335.947,295.134z"/> </g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg></span>';
+  downloadButton.className = shareButtons[1].className + ' downloadButton';
+  downloadButton.onclick = downloadVideo;
+  downloadButton.style.cursor = 'pointer';
+});
 
 function saveFile(blob, filename) {
   if (window.navigator.msSaveOrOpenBlob) {
@@ -47,29 +60,6 @@ async function downloadVideo(){
 	});
   const watermarklessVideo = await watermarklessVideoResponse.blob();
   
-  const splitWindowURL = window.location.toString().split('/');
-  const vidID = splitWindowURL[splitWindowURL.length - 1].slice(0, 18);
-  const fileName = vidID + '.mp4';
+  const fileName = window.location.href.match(/video\/(\d{19})/)[1] + '.mp4';
   saveFile(watermarklessVideo, fileName);
-}
-
-var pageObserver = new MutationObserver(createDownloadButton);
-pageObserver.observe(
-  document.body,
-  { attributes: true, childList: false, subtree: true }
-);
-
-
-async function createDownloadButton(){
-  const shareBoxes = document.getElementsByClassName('share-group');
-  for(shareBox of shareBoxes){
-    if(shareBox.getElementsByClassName('downloadButton').length > 0) continue;
-    let shareButtons = shareBox.getElementsByTagName('a');
-    let downloadButton = document.createElement('a');
-    shareBox.insertBefore(downloadButton, shareButtons[4]);
-    downloadButton.innerHTML = '<span><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"  viewBox="0 0 477.867 477.867" style="enable-background:new 0 0 477.867 477.867;" xml:space="preserve"><g> <g> <path d="M443.733,307.2c-9.426,0-17.067,7.641-17.067,17.067v102.4c0,9.426-7.641,17.067-17.067,17.067H68.267 c-9.426,0-17.067-7.641-17.067-17.067v-102.4c0-9.426-7.641-17.067-17.067-17.067s-17.067,7.641-17.067,17.067v102.4 c0,28.277,22.923,51.2,51.2,51.2H409.6c28.277,0,51.2-22.923,51.2-51.2v-102.4C460.8,314.841,453.159,307.2,443.733,307.2z"/> </g></g><g> <g> <path d="M335.947,295.134c-6.614-6.387-17.099-6.387-23.712,0L256,351.334V17.067C256,7.641,248.359,0,238.933,0 s-17.067,7.641-17.067,17.067v334.268l-56.201-56.201c-6.78-6.548-17.584-6.36-24.132,0.419c-6.388,6.614-6.388,17.099,0,23.713 l85.333,85.333c6.657,6.673,17.463,6.687,24.136,0.031c0.01-0.01,0.02-0.02,0.031-0.031l85.333-85.333 C342.915,312.486,342.727,301.682,335.947,295.134z"/> </g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg></span>';
-    downloadButton.className = shareButtons[1].className + ' downloadButton';
-    downloadButton.onclick = downloadVideo;
-    downloadButton.style.cursor = 'pointer';
-  }
 }
